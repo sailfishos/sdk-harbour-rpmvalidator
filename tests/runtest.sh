0ded5233d5370c3c14f92a956ce219bdb80b58df
Reto Zingg <reto.zingg@jolla.com>
2014-01-28 15:13:57 +0200

    remove generate script, integrate to runtest.sh
    generate_expected_output.sh and runtest.sh are pretty much the same,
    just the output dir of the rpmvalidator.sh differs and no asserts in
    generate_expected_output.sh. To lower the maintainance burden this
    is now merged and generating the expected output can be done with
    the option -g/--generate.
    
    Signed-off-by: Reto Zingg <reto.zingg@jolla.com>

--- tests/runtest.sh
+++ tests/runtest.sh
@@ -21,6 +21,42 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 
+usage() {
+    cat <<EOF
+Run some basic tests with rpmvalidation.sh
+
+Usage:
+   $0 [OPTION]
+
+Options:
+   -g | --generate      generate expected output files
+   -h | --help          this help
+
+EOF
+    # exit if any argument is given
+    [[ -n "$1" ]] && exit 1
+}
+
+function parseCommandLineOptions () {
+    OPT_GENERATE=0
+    # parse command line options
+    while [[ ${1:-} ]]; do
+        case "$1" in
+            -h | --help) shift
+                usage quit
+                ;;
+            -g | --generate) shift
+                OPT_GENERATE=1
+                ;;
+            -*)
+                usage quit
+                ;;
+            *)
+                ;;
+        esac
+    done
+}
+
 function oneTimeSetUp() {
     RPMVALITATOR="${SCRIPT_DIR}/../rpmvalidation.sh --sort --config-dir ${SCRIPT_DIR}/.."
     RPMS_DIR="${SCRIPT_DIR}/rpms"
@@ -31,75 +67,88 @@ function oneTimeSetUp() {
     HB_GOOD_x86_RPM="harbour-good-0.5-1.i586.rpm"
     HB_BAD_ARM_RPM="harbour-bad-0.1-1.armv7hl.rpm"
     HB_BAD_x86_RPM="harbour-bad-0.1-1.i586.rpm"
+    if [[ ${OPT_GENERATE} -eq 0 ]] ; then
+        # normal case for test runs
+        OUTPUT_DIR=${SHUNIT_TMPDIR}
+    else
+        # --generate is set, so EXPECTED_DIR = OUTPUT_DIR
+        # not used for real test run but to generate the
+        # expected output, when something changed
+        OUTPUT_DIR=${EXPECTED_DIR}
+    fi
 } 
 
 function testHarbourGoodArm() {
-    ${RPMVALITATOR} ${RPMS_DIR}/${HB_GOOD_ARM_RPM} 2>&1 | ${SED_FILTER} > ${SHUNIT_TMPDIR}/testHarbourGoodArm.txt 
-    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testHarbourGoodArm.txt ${SHUNIT_TMPDIR}/testHarbourGoodArm.txt
+    ${RPMVALITATOR} ${RPMS_DIR}/${HB_GOOD_ARM_RPM} 2>&1 | ${SED_FILTER} > ${OUTPUT_DIR}/testHarbourGoodArm.txt
+    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testHarbourGoodArm.txt ${OUTPUT_DIR}/testHarbourGoodArm.txt
     assertTrue 'Expected output differs.' $?
 }
 
 function testHarbourGoodArmNoColor() {
-    ${RPMVALITATOR} --no-color ${RPMS_DIR}/${HB_GOOD_ARM_RPM} 2>&1 | ${SED_FILTER} > ${SHUNIT_TMPDIR}/testHarbourGoodArmNoColor.txt 
-    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testHarbourGoodArmNoColor.txt ${SHUNIT_TMPDIR}/testHarbourGoodArmNoColor.txt
+    ${RPMVALITATOR} --no-color ${RPMS_DIR}/${HB_GOOD_ARM_RPM} 2>&1 | ${SED_FILTER} > ${OUTPUT_DIR}/testHarbourGoodArmNoColor.txt
+    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testHarbourGoodArmNoColor.txt ${OUTPUT_DIR}/testHarbourGoodArmNoColor.txt
     assertTrue 'Expected output differs.' $?
 }
 
 function testHarbourBadArm() {
-    ${RPMVALITATOR} ${RPMS_DIR}/${HB_BAD_ARM_RPM} 2>&1 | ${SED_FILTER} > ${SHUNIT_TMPDIR}/testHarbourBadArm.txt 
-    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testHarbourBadArm.txt ${SHUNIT_TMPDIR}/testHarbourBadArm.txt
+    ${RPMVALITATOR} ${RPMS_DIR}/${HB_BAD_ARM_RPM} 2>&1 | ${SED_FILTER} > ${OUTPUT_DIR}/testHarbourBadArm.txt
+    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testHarbourBadArm.txt ${OUTPUT_DIR}/testHarbourBadArm.txt
     assertTrue 'Expected output differs.' $?
 }
 
 function testHarbourBadArmNoColor() {
-    ${RPMVALITATOR} --no-color ${RPMS_DIR}/${HB_BAD_ARM_RPM} 2>&1 | ${SED_FILTER} > ${SHUNIT_TMPDIR}/testHarbourBadArmNoColor.txt 
-    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testHarbourBadArmNoColor.txt ${SHUNIT_TMPDIR}/testHarbourBadArmNoColor.txt
+    ${RPMVALITATOR} --no-color ${RPMS_DIR}/${HB_BAD_ARM_RPM} 2>&1 | ${SED_FILTER} > ${OUTPUT_DIR}/testHarbourBadArmNoColor.txt
+    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testHarbourBadArmNoColor.txt ${OUTPUT_DIR}/testHarbourBadArmNoColor.txt
     assertTrue 'Expected output differs.' $?
 }
 
 function testHarbourGoodIntel() {
-    ${RPMVALITATOR} ${RPMS_DIR}/${HB_GOOD_x86_RPM} 2>&1 | ${SED_FILTER} > ${SHUNIT_TMPDIR}/testHarbourGoodIntel.txt 
-    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testHarbourGoodIntel.txt ${SHUNIT_TMPDIR}/testHarbourGoodIntel.txt
+    ${RPMVALITATOR} ${RPMS_DIR}/${HB_GOOD_x86_RPM} 2>&1 | ${SED_FILTER} > ${OUTPUT_DIR}/testHarbourGoodIntel.txt
+    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testHarbourGoodIntel.txt ${OUTPUT_DIR}/testHarbourGoodIntel.txt
     assertTrue 'Expected output differs.' $?
 }
 
 function testHarbourBadIntel() {
-    ${RPMVALITATOR} ${RPMS_DIR}/${HB_BAD_x86_RPM} 2>&1 | ${SED_FILTER} > ${SHUNIT_TMPDIR}/testHarbourBadIntel.txt 
-    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testHarbourBadIntel.txt ${SHUNIT_TMPDIR}/testHarbourBadIntel.txt
+    ${RPMVALITATOR} ${RPMS_DIR}/${HB_BAD_x86_RPM} 2>&1 | ${SED_FILTER} > ${OUTPUT_DIR}/testHarbourBadIntel.txt
+    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testHarbourBadIntel.txt ${OUTPUT_DIR}/testHarbourBadIntel.txt
     assertTrue 'Expected output differs.' $?
 }
 
 function testBadFileName() {
-    ${RPMVALITATOR} ${RPMS_DIR}/bad-file-name.rpm 2>&1 | ${SED_FILTER} > ${SHUNIT_TMPDIR}/testBadFileName.txt 
-    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testBadFileName.txt ${SHUNIT_TMPDIR}/testBadFileName.txt
+    ${RPMVALITATOR} ${RPMS_DIR}/bad-file-name.rpm 2>&1 | ${SED_FILTER} > ${OUTPUT_DIR}/testBadFileName.txt
+    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testBadFileName.txt ${OUTPUT_DIR}/testBadFileName.txt
     assertTrue 'Expected output differs.' $?
 }
 
 function testNotAnRpm() {
-    ${RPMVALITATOR} ${RPMS_DIR}/some-random-file.rpm 2>&1 | ${SED_FILTER} > ${SHUNIT_TMPDIR}/testNotAnRpm.txt 
-    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testNotAnRpm.txt ${SHUNIT_TMPDIR}/testNotAnRpm.txt
+    ${RPMVALITATOR} ${RPMS_DIR}/some-random-file.rpm 2>&1 | ${SED_FILTER} > ${OUTPUT_DIR}/testNotAnRpm.txt
+    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testNotAnRpm.txt ${OUTPUT_DIR}/testNotAnRpm.txt
     assertTrue 'Expected output differs.' $?
 }
  
 function testBadFileNameNoColor() {
-    ${RPMVALITATOR} --no-color ${RPMS_DIR}/bad-file-name.rpm 2>&1 | ${SED_FILTER} > ${SHUNIT_TMPDIR}/testBadFileNameNoColor.txt 
-    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testBadFileNameNoColor.txt ${SHUNIT_TMPDIR}/testBadFileNameNoColor.txt
+    ${RPMVALITATOR} --no-color ${RPMS_DIR}/bad-file-name.rpm 2>&1 | ${SED_FILTER} > ${OUTPUT_DIR}/testBadFileNameNoColor.txt
+    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testBadFileNameNoColor.txt ${OUTPUT_DIR}/testBadFileNameNoColor.txt
     assertTrue 'Expected output differs.' $?
 }
 
 function testNotAnRpmNoColor() {
-    ${RPMVALITATOR} --no-color ${RPMS_DIR}/some-random-file.rpm 2>&1 | ${SED_FILTER} > ${SHUNIT_TMPDIR}/testNotAnRpmNoColor.txt 
-    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testNotAnRpmNoColor.txt ${SHUNIT_TMPDIR}/testNotAnRpmNoColor.txt
+    ${RPMVALITATOR} --no-color ${RPMS_DIR}/some-random-file.rpm 2>&1 | ${SED_FILTER} > ${OUTPUT_DIR}/testNotAnRpmNoColor.txt
+    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testNotAnRpmNoColor.txt ${OUTPUT_DIR}/testNotAnRpmNoColor.txt
     assertTrue 'Expected output differs.' $?
 }
 
 function testHarborBadArm() {
-    ${RPMVALITATOR} ${RPMS_DIR}/harbor-bad-0.1-1.armv7hl.rpm 2>&1 | ${SED_FILTER} > ${SHUNIT_TMPDIR}/testHarborBadArm.txt 
-    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testHarborBadArm.txt ${SHUNIT_TMPDIR}/testHarborBadArm.txt
+    ${RPMVALITATOR} ${RPMS_DIR}/harbor-bad-0.1-1.armv7hl.rpm 2>&1 | ${SED_FILTER} > ${OUTPUT_DIR}/testHarborBadArm.txt
+    diff ${DIFF_OPTS} ${EXPECTED_DIR}/testHarborBadArm.txt ${OUTPUT_DIR}/testHarborBadArm.txt
     assertTrue 'Expected output differs.' $?
 }
 
 ## Call and Run all Tests
+parseCommandLineOptions $@
 SCRIPT_DIR=$(dirname $(readlink -f $0))
+# the positional parameters are unset ($@, $#, $*)
+# we don't want them to be passed to shunit2 so it works in source mode
+set --
 . "${SCRIPT_DIR}/shunit2/shunit2"
 
